7996a85267109c58abf10f0991f4d11f6e791b6a
Piotr Kapski <kapski.p@gmail.com>
2021-03-22 18:25:21 +0100

    Added JavaFX view, added some input data.

--- src/main/java/VRP_Main.java
+++ src/main/java/VRP_Main.java
@@ -1,3 +1,11 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
 import pl.polsl.model.Distance;
 import pl.polsl.model.Node;
 import pl.polsl.model.SolutionMethod;
@@ -11,20 +19,25 @@ import pl.polsl.utils.files.FilesManager;
 
 import java.io.IOException;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class VRP_Main {
+public class VRP_Main extends Application {
 
     public static final String NUMBER_OF_VEHICLES = "numberOfVehicles";
     public static final String VEHICLE_CAPACITY = "vehicleCapacity";
     public static final String START_HOUR = "startHour";
     public static final String METHOD = "method";
+    private static SolutionResults results;
+    private static List<Node> nodes;
 
     public static void main(String[] args) throws IOException {
         FilesManager filesManager = new FilesManager();
         ConsoleUtils consoleUtils = new ConsoleUtils();
         DataUtils dataUtils = new DataUtils();
-        List<Node> nodes = filesManager.loadNodesFromCSV("src/main/resources/initialData/inputCSVData.csv");
+        nodes = filesManager.loadNodesFromCSV("src/main/resources/initialData/inputCSVData.csv");
         Distance[][] distances = dataUtils.calculateNodeDistances(nodes);
         int numberOfNodes = distances.length;
         int numberOfVehicles = Integer.parseInt(System.getProperty(NUMBER_OF_VEHICLES, "4"));
@@ -44,9 +57,56 @@ public class VRP_Main {
             default:
                 throw new IllegalStateException("Unexpected value: " + System.getProperty(METHOD));
         }
-        SolutionResults results = strategy.getSolution(nodes, distances, numberOfVehicles, vehicleCapacity, startingTime);
+        results = strategy.getSolution(nodes, distances, numberOfVehicles, vehicleCapacity, startingTime);
         consoleUtils.printResults(results);
+        launch();
     }
 
 
+    @Override
+    public void start(Stage stage) {
+        Map<Integer, Node> nodesMap = getNodesMap();
+        Group root = new Group();
+        Circle circle = createCircle(nodes.get(0), Color.RED);
+        root.getChildren().add(circle);
+        for (ArrayList<Integer> route : results.getRoutesMap().values()) {
+            if (route.size() == 1) {
+                continue;
+            }
+
+            for (int i = 0; i < route.size() - 1; i++) {
+                if (route.get(i + 1) != 0) {
+                    circle = createCircle(nodesMap.get(route.get(i + 1)), Color.BLACK);
+                    root.getChildren().add(circle);
+                }
+                Line line = new Line();
+                line.setStartX(nodesMap.get(route.get(i)).getX());
+                line.setStartY(nodesMap.get(route.get(i)).getY());
+                line.setEndX(nodesMap.get(route.get(i + 1)).getX());
+                line.setEndY(nodesMap.get(route.get(i + 1)).getY());
+                root.getChildren().add(line);
+            }
+        }
+
+        Scene scene = new Scene(root, 800, 800);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private Map<Integer, Node> getNodesMap() {
+        Map<Integer, Node> nodesMap = new HashMap<>();
+        for (Node node : nodes) {
+            nodesMap.put(node.getId(), node);
+        }
+        return nodesMap;
+    }
+
+    private Circle createCircle(Node node, Paint color) {
+        Circle circle = new Circle();
+        circle.setCenterX(node.getX());
+        circle.setCenterY(node.getY());
+        circle.setRadius(5.0f);
+        circle.setFill(color);
+        return circle;
+    }
 }
